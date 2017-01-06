c5df34529a22dd5638040247f1a91ae1e4456e99
Mannah <kenikall@gmail.com>
2017-01-06 15:45:50 -0800

    refactoring email

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,2 +1,27 @@
+require 'net/smtp'
+
 module IndexHelper
+
+
+  def send_email(to,opts={})
+    opts[:server]      ||= 'localhost'
+    opts[:subject]     ||= "You need to see this"
+    opts[:header]        ||= "Important stuff!"
+    opts[:body]        ||= "Important stuff!"
+    opts[:footer]        ||= "Important stuff!"
+
+    msg = <<END_OF_MESSAGE
+  From: #{K. Mannah Kallon} <#{mannahcodes@gmail.com}>
+  To: <#{to}>
+  Subject: #{opts[:subject]}
+
+  #{opts[:header]}
+  #{opts[:body]}
+  #{opts[:footer]}
+  END_OF_MESSAGE
+
+    Net::SMTP.start(opts[:server]) do |smtp|
+      smtp.send_message msg, opts[:from], to
+    end
+  end
 end
